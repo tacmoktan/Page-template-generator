6475d9d7e0d2a62d99ab17ae5003fdeb5f8531d5
Tashi <tacmoktan@gmail.com>
2020-02-11 17:21:17 +0545

    Add logic to generate img from page template

--- src/Components/TemplateForm.js
+++ src/Components/TemplateForm.js
@@ -1,13 +1,15 @@
 import React, { useState } from 'react';
-//component
-import Template from './Template';
+import ReactDOMServer from 'react-dom/server';
+import domtoimage from 'dom-to-image';
+import { saveAs } from 'file-saver';
 
 const TemplateForm = () => {
 
     const [data, setData] = useState({
         title: "",
         body: "",
-        img: {}
+        img: {},
+        isDataSubmitted: false
     })
 
     const handleInput = field => e =>
@@ -19,15 +21,37 @@ const TemplateForm = () => {
     const handleImageInput = () => e => {
         const reader = new FileReader();
 
-        reader.onloadend = () => setData({ ...data, img: reader.result });      
+        reader.onloadend = () => setData({ ...data, img: reader.result });
         let imageFiles = e.target.files;
         reader.readAsDataURL(imageFiles[0]);
     }
 
-    React.useEffect(() => console.log(data));
+    //convert JSX to HTML 
+    let pageTemplate = (
+        <div className="container" id="template-container">
+            {Object.keys(data.img).length !== 0 && (
+                <div className="img-container">
+                    <img src={data.img} alt="img for template" className="template-image" />
+                </div>)}
+            {data.title && <h1 className="template-heading">{data.title}</h1>}
+            {data.body && <p className="templatedata.-body">{data.body}</p>}
+        </div>
+    );
+
+    const htmlString = ReactDOMServer.renderToStaticMarkup(pageTemplate);       //converts JSX to html string
+    
+    let htmlPageTemplate = document.createElement('div');
+    htmlPageTemplate.innerHTML = htmlString;
+    let htmlTemplateContainer = htmlPageTemplate.querySelector('#template-container')  
 
     const handleSubmit = e => {
         e.preventDefault();
+        setData({ ...data, isDataSubmitted: true });
+
+        domtoimage.toBlob(htmlTemplateContainer, { width: 1080, height: 960 })
+            .then(function (blob) {
+                window.saveAs(blob, "page-template.png");
+            });
         console.log(data);
     }
 
@@ -54,7 +78,8 @@ const TemplateForm = () => {
                 <button type="submit" className="button" >SUBMIT</button>
             </form>
 
-            <Template {...data} />
+            {data.isDataSubmitted && pageTemplate}
+
         </>
     );
 }
